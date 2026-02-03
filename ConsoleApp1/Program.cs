85edbab2bebb49730c38322a20570e4114eed2aa
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-03 10:10:36 +0430

    Understant assignment error

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,3 @@
-﻿// Float literal
-Console.Write(123.123F);
-// Double literal (You don't add a suffix for double)
-Console.Write(123.123);
-// Decimal literal
-Console.Write(123.123M);
+﻿string firstname;
+
+"John" = firstname;
