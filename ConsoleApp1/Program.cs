10d93a6f22f9deb13f44a8c9141ad5457394a7d3
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-03 09:51:03 +0430

    Use Float literals

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.Write('Hasd');
+﻿// Float literal
+Console.Write(123.123F);
+// Double literal (You don't add a suffix for double)
+Console.Write(123.123);
+// Decimal literal
+Console.Write(123.123M);
