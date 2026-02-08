3cf9353f5c5c17b355890b0615f9a042449ccfef
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-08 16:28:54 +0430

    Use Replace and Trim

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,9 @@
 ﻿string firstFriend = "   Scott";
 string secondFriend = "Chris   ";
 
-Console.WriteLine($"My friends are {firstFriend.Trim()} and {secondFriend.Trim()}.");
+string Friend = $"My friends are {firstFriend.Trim()} and {secondFriend.Trim()}.";
+//it changes all of the occurrences of friends to Buddies
+Console.WriteLine(Friend.Replace("friends", "Buddies"));
+// The original variable is not changed
+Console.Write(Friend);
 
