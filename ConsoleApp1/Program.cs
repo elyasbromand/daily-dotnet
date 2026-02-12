a2a6fb655c7ce5088cdd71203316f272db985eda
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-12 11:07:22 +0430

    double float errors

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
-﻿int x = 2100000000;
-int y = 2100000000;
-int w = 2100000000;
-int main = 210000000;
-double main2 = 21.21;
-long z = checked(x+y);
+﻿double x = 21.43; //Natural type
+float y = 12.12; //This needs conversion, since it cannot convert float to double implicitly
+long z = checked(x + y);
 Console.Write(z);
