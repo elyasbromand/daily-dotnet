418924ea13d2d07ffec3406d583020cec0243b79
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-14 12:50:22 +0430

    Use List<T>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,16 @@
-﻿decimal x = 21.43M; //Explicit type
-decimal y = 12.12M; //Explicit type
-long z = checked(x + y);
-Console.Write(z);
+﻿//List<T> being prounouced as list of T, where T is the type of elements in the list. It is a generic collection that can hold any type of data. The list provides various methods to manipulate the data, such as adding, removing, and searching for elements. It is a dynamic array that can grow or shrink in size as needed. The list is part of the System.Collections.Generic namespace in C#.
+
+
+
+List<string> names = ["Ahmad", "Jourdan"];
+
+names.Add("Walid");
+names.Remove("Ahmad");
+names.Add("Ali");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.Write(names[0]);
