6b9e6467fcefee94d7f45b07e8fee65b4e236469
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-16 14:22:43 +0430

    Add obstraction and overriding methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,29 @@ var p1 = new Person("John", "Doe", new DateOnly(1990, 1, 1));
 
 Console.WriteLine($"Name: {p1.First} {p1.Last}, Birth Date: {p1.Bd}");
 
+Cat catie = new Cat("Catie");
+Console.WriteLine($"{catie.First} says {catie.MakeSound()}");
+
 public class Person (string firstName, string lastName, DateOnly birthDate)
 {
     public string First { get; } = firstName;
     public string Last { get;  } = lastName;
     public DateOnly Bd { get; } = birthDate;
 }
+
+
+public abstract class Pet (string firstname)
+{
+    public string First { get; } = firstname;
+
+    public abstract string MakeSound();
+
+}
+
+
+public class Cat(string f) : Pet (f)
+{
+    public override string MakeSound() => "Meow";
+}
+
+
