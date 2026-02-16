0e5ba53794a8d7295dad6da727eb9845d96d87df
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-16 14:44:26 +0430

    Proper document it

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,16 +2,18 @@ Console.WriteLine("Hello, OOP!");
 // new way of creating class and object
 var p1 = new Person("John", "Doe", new DateOnly(1990, 1, 1));
 
-Console.WriteLine($"Name: {p1.First} {p1.Last}, Birth Date: {p1.Bd}");
+p1.Pets.Add(new Cat("Fluffy"));
 
-Cat catie = new Cat("Catie");
-Console.WriteLine($"{catie.First} says {catie.MakeSound()}");
+Console.WriteLine($"Name: {p1.First} {p1.Last}, Birth Date: {p1.Bd}, Pets: {p1.Pets[0].First}");
+
 
 public class Person (string firstName, string lastName, DateOnly birthDate)
 {
     public string First { get; } = firstName;
     public string Last { get;  } = lastName;
     public DateOnly Bd { get; } = birthDate;
+
+    public List<Pet> Pets { get; } = [];
 }
 
 
@@ -23,10 +25,15 @@ public abstract class Pet (string firstname)
 
 }
 
-
+// send the f to the base class constructor to become as First in the pet class.
 public class Cat(string f) : Pet (f)
 {
     public override string MakeSound() => "Meow";
+
 }
 
+public class Dog(string f) : Pet(f)
+{
+    public override string MakeSound() => "Woof";
 
+}
