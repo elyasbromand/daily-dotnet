63505460a8bd8d3096ec43caf45362dce4fcb8e9
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-16 12:29:15 +0430

    IEnumberable and List difference explained

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,16 @@
 // Specify the data source.
 List<int> scores = [97, 92, 81, 60];
 
-
-return;
-
-// Define the query expression.
+// Define the query expression. it is deffered execution, it is not executed until the foreach statement is executed.
+// LINQ returns an IEnumerable<T> and it is foreachable.
 IEnumerable<int> scoreQuery =
     from score in scores
     where score > 80
     select score;
 
-// Execute the query.
+scores.Add(99); // this will also be included in the query because the query is not executed until the foreach statement is executed.
+
+// Execute the query. Now the LINQ is executed. 
 foreach (var i in scoreQuery)
 {
     Console.Write(i + " ");
