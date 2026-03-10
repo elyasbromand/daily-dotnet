2c910f10b41d5c5342dd328f59e8b311f16c7826
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-03-10 23:44:14 +0430

    Use XML commenting, and disable and enable warnings using #pragma

--- P1/ConsoleApp1/Program.cs
+++ P1/ConsoleApp1/Program.cs
@@ -1,4 +1,17 @@
 ﻿string? userInput = Console.ReadLine();
 
-Console.Write(userInput);
+Write(userInput);
 
+//<summary>
+//this method writes the input to the console
+//</summary>
+static void Write(string? input)
+{
+    Console.WriteLine(input);
+}
+
+#pragma warning disable CS0168
+int unusedVar;
+#pragma warning restore CS0168
+
+System.Console.WriteLine();
