d99df2d9722051c6aa019c9c0e9fd838cee14699
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-03-12 12:03:18 +0430

    Apply Casting

---
+++ P2/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// implicit conversion
+
+int myInt = 1123123123;
+
+double myDouble = myInt;
+
+long myLong = myInt;
+
+// explicit conversion - casting
+
+myLong = 1210000012312312112;
+
+int myInt2 = (int)myLong;
+
+myInt2 = (int)myDouble;
