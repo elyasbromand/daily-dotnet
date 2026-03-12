49318a3847e00e257b2a3cc5a285b3cc2b795ec2
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-03-12 12:04:26 +0430

    Commit New changes

--- P1/ConsoleApp1/Program.cs
+++ P1/ConsoleApp1/Program.cs
@@ -1,17 +1,4 @@
-﻿string? userInput = Console.ReadLine();
+﻿float myNumber = 0;
+myNumber = float.Parse(Console.ReadLine());
 
-Write(userInput);
-
-//<summary>
-//this method writes the input to the console
-//</summary>
-static void Write(string? input)
-{
-    Console.WriteLine(input);
-}
-
-#pragma warning disable CS0168
-int unusedVar;
-#pragma warning restore CS0168
-
-System.Console.WriteLine();
+Console.Write(myNumber);
