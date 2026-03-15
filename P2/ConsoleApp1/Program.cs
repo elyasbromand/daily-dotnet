29cfc9e1499e9e1d321c2fb1d56890cdfc2d8943
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-03-16 02:52:16 +0430

    lamda variable scope

--- P2/ConsoleApp1/Program.cs
+++ P2/ConsoleApp1/Program.cs
@@ -1,23 +1,32 @@
-﻿// needs to be either "true" or "false"
-string myBoolString = "TRUE";
+﻿// // needs to be either "true" or "false"
+// string myBoolString = "TRUE";
 
-bool myBool = Convert.ToBoolean(myBoolString);
+// bool myBool = Convert.ToBoolean(myBoolString);
 
-Console.Write(myBool);
+// Console.Write(myBool);
 
-int num1 = 5;
+// int num1 = 5;
 
-int num2 = int.Parse(Console.ReadLine() ?? "0");
+// int num2 = int.Parse(Console.ReadLine() ?? "0");
 
-System.Console.WriteLine("Addition num1 and num2 is " + num1 * num2);
+// System.Console.WriteLine("Addition num1 and num2 is " + num1 * num2);
 
-if (true && true)
-{
-    System.Console.WriteLine("And is working");
-}
+// if (true && true)
+// {
+//     System.Console.WriteLine("And is working");
+// }
 
-//XOR is true if one of the values is true but not both
-if (true ^ false)
-{
-    System.Console.WriteLine("Xor Working");
-}
+// //XOR is true if one of the values is true but not both
+// if (true ^ false)
+// {
+//     System.Console.WriteLine("Xor Working");
+// }
+
+// lambda expression captures the variable x by reference, so when x is updated to 20, the lambda will return the updated value. Therefore, the output of the code will be: 20
+int x = 10;
+
+Func<int> f = () => x;
+
+x = 20;
+
+Console.WriteLine(f());
