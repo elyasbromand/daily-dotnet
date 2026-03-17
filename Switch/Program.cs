cda95609510faa9cc47123661ca6b5915564410e
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-17 20:32:54 +0430

    New Switch statement syntax

---
+++ Switch/Program.cs
@@ -0,0 +1,25 @@
+﻿string monthName;
+
+int month = int.Parse(Console.ReadLine());
+
+//New switch expression syntax
+monthName = month switch
+{
+    1 => "January",
+    2 => "February",
+    _ => "Invalid month",
+};
+
+//Old switch statement syntax
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    default:
+        monthName = "Invalid month";
+        break;
+};
