03ded300454906533767ebe97a2084747d68839f
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-17 22:12:13 +0430

    Create Thread progress app

---
+++ Thread Progress/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Progress: 0%");
+for (int i = 1; i <= 100; i++)
+{
+    // Simulate work
+    Thread.Sleep(50);
+    // Overwrites the previous output on the same line
+    Console.Write($"\rProgress: {i}%");
+}
+Console.WriteLine("\nCompleted!");
