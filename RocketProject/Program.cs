1f84c0ba8aea1b0328ab9b6c1df9bc941ab6a6b5
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-21 22:46:32 +0430

    Rocket Landing project

---
+++ RocketProject/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Rocket Launcher project");
+
+string rocket = "/\\\n||\n||\n||\n/\\";
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Clear();
+    Console.WriteLine(rocket);
+    rocket = "\n" + rocket;
+    Thread.Sleep(1000);
+}
+
+System.Console.WriteLine("-----------------------------\n rocket landed successfully!");
