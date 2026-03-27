bdc1da48873bbc7cf8d71de690fe423f97094317
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-27 14:44:28 +0430

    Compile time error - Method inside Main

--- P1/ConsoleApp1/Program.cs
+++ P1/ConsoleApp1/Program.cs
@@ -1,6 +1,10 @@
-﻿void printString(int str)
-{
-    Console.WriteLine(str);
-}
+﻿Program myProgram = new();
 
-printString(123);
+// The following line will cause a compile-time error because the Run method is inside Main method, not in the Program class.
+// myProgram.Run();
+
+int Run()
+{
+    System.Console.WriteLine("asdf");
+    return 0;
+}
