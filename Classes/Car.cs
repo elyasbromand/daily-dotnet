8f44e147c27c1db8044bb5ae29e3a167c40d5b96
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-30 00:40:15 +0430

    Manual wrapping members with property

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -2,7 +2,15 @@ namespace Classes
 {
     internal class Car(String model = "unknown")
     {
-        // This is Auto implemented property, it will automatically create a private field to store the value of the property
-        public string Model { get; set; } = model;
+        // This is Auto implemented property, it will automatically create a private field to store the value of the property. and at first it will take the constructor parameter and assign it to the property. 
+        // public string Model { get; set; } = model;
+
+        // You can also do it manually by creating a private field and a public property to access it
+        private string _model = model;
+        public string Model
+        {
+            get => _model;
+            set => _model = value;
+        }
     }
 }
