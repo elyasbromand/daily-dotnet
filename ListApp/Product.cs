ce02cd2a638757c98f1122ba4e6989adcb48b978
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-04-06 23:25:04 +0430

    Use lists with Complex objects

---
+++ ListApp/Product.cs
@@ -0,0 +1,8 @@
+namespace ListApp
+{
+    internal class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+    }
+}
--- ListApp/Program.cs
+++ ListApp/Program.cs
@@ -4,33 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            // old way
-            List<string> names = new List<string>() { "John", "Jane", "Doe", "john" };
-            // new way
-            List<string> strings = ["asdf", "fdsa", "qwerty", "ytrewq"];
+            List<Product> productsList = [];
 
-            // showing list in names format:
-            Console.WriteLine("Names: " + string.Join(", ", names));
+            productsList.Add(new Product { Name = "Laptop", Price = 1000 });
+            productsList.Add(new Product { Name = "Desktop", Price = 2000 });
 
-            names.Add("Adam smith");
-
-            names.RemoveAll(name => name.Equals("John", StringComparison.OrdinalIgnoreCase));
-
-            // let's use predicate to check names which has length greater than 4
-
-            Predicate<string> hasLengthGreaterThan4 = name => name.Length > 5;
-
-            List<string> longNames = names.FindAll(hasLengthGreaterThan4);
-
-            foreach (string name in longNames)
+            foreach (var product in productsList)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(product);
             }
-
-            // Modern Delegate doesn't use Predicate for returning boolean values, instead it uses Func<T, bool> which is more flexible and can be used for various types of delegates.
-            Func<int, bool> check = x => x > 5;
-            Func<int, int> square = x => x * x;
-            Func<int, string> text = x => $"Number: {x}";
         }
     }
 }
