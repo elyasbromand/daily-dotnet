1a068132f6c9302005d1ee9f780c7b8a5729dec7
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-04-06 16:38:06 +0430

    Use Predicate with Lambda

--- ListApp/Program.cs
+++ ListApp/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             // old way
-            List<string> names = new List<string>() { "John", "Jane", "Doe" };
+            List<string> names = new List<string>() { "John", "Jane", "Doe", "john" };
             // new way
             List<string> strings = ["asdf", "fdsa", "qwerty", "ytrewq"];
 
@@ -13,7 +13,19 @@
             Console.WriteLine("Names: " + string.Join(", ", names));
 
             names.Add("Adam smith");
+
+            names.RemoveAll(name => name.Equals("John", StringComparison.OrdinalIgnoreCase));
+
+            // let's use predicate to check names which has length greater than 4
+
+            Predicate<string> hasLengthGreaterThan4 = name => name.Length > 5;
+
+            List<string> longNames = names.FindAll(hasLengthGreaterThan4);
+
+            foreach (string name in longNames)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
-    
