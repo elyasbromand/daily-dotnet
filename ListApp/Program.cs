38e071d6ffc2def889d8c84a2b4928c116754a51
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-04-06 23:51:30 +0430

    Use IEnumerable in where method in lists

--- ListApp/Program.cs
+++ ListApp/Program.cs
@@ -4,10 +4,25 @@
     {
         public static void Main(string[] args)
         {
-            List<Product> productsList = [];
+            List<Product> productsList =
+            [
+                new Product { Name = "A", Price = 1000 },
+                new Product { Name = "B", Price = 2000 },
+                new Product { Name = "C", Price = 3000 },
+            ];
 
-            productsList.Add(new Product { Name = "Laptop", Price = 1000 });
-            productsList.Add(new Product { Name = "Desktop", Price = 2000 });
+            // the Where method will return a IEnummerable which we then cast it to a list since we are storing it in a list of type Product
+
+            List<Product> cheapProduct = productsList
+                .Where(product => product.Price <= 2000)
+                .ToList();
+
+            // You can also do it the modern way:
+            /*
+            List<Product> cheapProduct = [..productsList
+                .Where(product => product.Price <= 2000)
+                .ToList(); ]
+            */
 
             foreach (var product in productsList)
             {
