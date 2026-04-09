6de9a1fb8300bc0404b3d98f3a934186bc29e2d5
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-04-10 02:52:26 +0430

    Manage multiple exceptions

--- tryCatch/Program.cs
+++ tryCatch/Program.cs
@@ -1,12 +1,47 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
 
-try
+namespace tryCatch
 {
-    int x = 0;
-    int y = 0;
-    System.Console.WriteLine();
-}
-catch (Exception ex)
-{
-    Debug.WriteLine(ex.ToString());
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int? number;
+            try
+            {
+                number = int.Parse(Console.ReadLine()!);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Inconsistent input stream");
+                Debug.WriteLine(ex.Message);
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine("You ran out of memory, try to free some.");
+                Debug.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Value is out of integer range");
+                Debug.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Pass a value.");
+                Debug.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Enter a valid integer");
+                Debug.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Cast resulted to overflow");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
 }
