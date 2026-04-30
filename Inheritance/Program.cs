c4181cd7d4eb9dd107c8ee396c00f8c62376890b
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-04-30 18:05:40 +0430

    Add method overriding

---
+++ Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Inheritance;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal dog = new Dog();
+        dog.Sound();
+    }
+}
+
+public class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void Sound()
+    {
+        base.Sound();
+    }
+}
