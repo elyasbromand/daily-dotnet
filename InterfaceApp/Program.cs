cf8ce92ae83010d6eb5d6913f0eaad7a48bc1a2e
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-05-03 17:09:51 +0430

    Add props to the interface

--- InterfaceApp/Program.cs
+++ InterfaceApp/Program.cs
@@ -2,11 +2,18 @@
 
 public interface IAnimal
 {
+    public int MyProperty { get; set; }
     void MakeSound();
 }
 
 public class Dog : IAnimal
 {
+    public int MyProperty
+    {
+        get;
+        set => throw new NotImplementedException();
+    }
+
     public void MakeSound()
     {
         throw new WhyAreYouNotImplementingException("Stupid bitch, implement the method");
