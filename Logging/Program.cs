c880723d25721669e3f7c6fa450519a16b4d188b
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-05-05 16:10:37 +0430

    Append logs to log.txt file

--- Logging/Program.cs
+++ Logging/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Logging;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        File.AppendAllText("log.txt", $"Log entry at {DateTime.Now} \n");
+        Console.WriteLine("Log entry written to log.txt");
+    }
+}
